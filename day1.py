1c78eca3ae2885e32866226a938e6371a66a6438
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-12-01 19:12:12 +0000

    Add 'smart' solution

--- day1.py
+++ day1.py
@@ -1,7 +1,10 @@
-from itertools import pairwise
+from itertools import pairwise, islice
+from collections import deque
 
 with open("day1_input.txt") as lines:
     data = [int(x) for x in lines.readlines()]
+
+### Simple Solution
 
 # Part 1
 print(sum(1 for a, b in pairwise(data) if b > a))
@@ -12,3 +15,20 @@ def triplet_sums(iterable):
         yield a + b + c
 
 print(sum(1 for a, b in pairwise(triplet_sums(data)) if int(b) > int(a)))
+
+
+### Overcomplicated Solution
+def sliding_window(iterable, n):
+    it = iter(iterable)
+    window = deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+# Part 1
+print(sum(1 for a, *_, b in sliding_window(data, 2) if b > a))
+
+# Part 2
+print(sum(1 for a, *_, b in sliding_window(data, 4) if b > a))
