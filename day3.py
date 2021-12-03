119559c7df655e9f1c4fc8b8356bb2e3e023f7e5
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-12-03 19:37:09 +0000

    Made day3 LEAN

--- day3.py
+++ day3.py
@@ -1,9 +1,6 @@
 from collections import Counter
-
-def parse_input():
-    with open("day3_input.txt") as f:
-        for line in f:
-            yield line.strip()
+with open("day3_input.txt") as f:
+    data = tuple(line.strip() for line in f)
 
 def bit_counts(values):
     bins = [Counter() for _ in range(12)]
@@ -12,24 +9,16 @@ def bit_counts(values):
             bins[idx][bit] += 1
     return bins
 
-# Part 1
-gamma_bits = [b.most_common()[0][0] for b in bit_counts(parse_input())]
-gamma_val = int("".join(gamma_bits), base=2)
-epslion_val = int("111111111111", base=2) - gamma_val
-print(gamma_val * epslion_val)
-
-# Part 2
 def filter_by_criteria(values, bit_criteria):
     for bit in range(12):
         counts = bit_counts(values)[bit]
-        values = [v for v in values if v[bit] == bit_criteria(counts)]
-        if len(values) == 1:
-            break
-    return int(values[0], base=2)
+        if len(values := [v for v in values if v[bit] == bit_criteria(counts)]) == 1:
+            return int(values[0], base=2)
 
-oxy_bit_criteria = lambda counts: "1" if counts["1"] >= counts["0"] else "0"
-co2_bit_criteria = lambda counts: "0" if counts["0"] <= counts["1"] else "1"
+gamma_bits = [b.most_common()[0][0] for b in bit_counts(data)]
+gamma_val = int("".join(gamma_bits), base=2)
+print("Part 1:", gamma_val * (int("1"*12, base=2) - gamma_val))
 
-oxy = filter_by_criteria(list(parse_input()), oxy_bit_criteria)
-co2 = filter_by_criteria(list(parse_input()), co2_bit_criteria)
-print(oxy * co2)
+oxy = filter_by_criteria(data, lambda c: "1" if c["1"] >= c["0"] else "0")
+co2 = filter_by_criteria(data, lambda c: "0" if c["0"] <= c["1"] else "1")
+print("Part 2:", oxy * co2)
