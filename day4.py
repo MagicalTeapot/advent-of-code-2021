1f2bead961f7bb8766d5423c843444110d42e095
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-12-04 20:53:06 +0000

    Reduce length by 1

--- day4.py
+++ day4.py
@@ -1,14 +1,4 @@
 from collections import defaultdict
-with open("day4_input.txt") as f:
-    balls = [int(x) for x in next(f).split(",")]
-    boards = []
-    for _ in f:
-        board = {}
-        for i in range(5):
-            for j, val in enumerate(next(f).split()):
-                board[int(val)] = (i, j)
-        boards.append(board)
-
 def solve_board(board, balls):
     lines = defaultdict(int)
     for ball_count, ball in enumerate(balls):
@@ -19,6 +9,15 @@ def solve_board(board, balls):
             if lines[f"r{i}"] == 5 or lines[f"c{j}"] == 5:
                 return (ball_count, ball * sum(board.keys()))
 
-results = [solve_board(b, balls) for b in boards]
+with open("day4_input.txt") as f:
+    balls = [int(x) for x in next(f).split(",")]
+    results = []
+    for _ in f:
+        board = {}
+        for i in range(5):
+            for j, val in enumerate(next(f).split()):
+                board[int(val)] = (i, j)
+        results.append(solve_board(board, balls))
+
 print("Part 1:", min(results, key=lambda x: x[0])[1])
 print("Part 2:", max(results, key=lambda x: x[0])[1])
